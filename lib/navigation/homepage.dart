50bfd705935787b6b322e2095836e0d99e7b5341
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-04-25 14:31:18 +0700

    membuat class HomePage

---
+++ lib/navigation/homepage.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:ucp1/navigation/piketpage.dart';
+import 'package:ucp1/navigation/pelangganpage.dart';
+import 'package:ucp1/navigation/pendataanpage.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomepageState();
+}
