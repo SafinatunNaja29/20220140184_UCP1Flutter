a0982e5e819b720caeaa2183848ccdbabee22f3b
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-04-25 14:01:27 +0700

    membuat form untuk password

--- lib/navigation/loginpage.dart
+++ lib/navigation/loginpage.dart
@@ -39,4 +39,14 @@ class _LoginPageState extends State<LoginPage> {
                   return null;
                 },
               ),
-              
+              TextFormField(  
+                controller: passwordController,
+                decoration: const InputDecoration(labelText: 'Password'),
+                obscureText: true, 
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+              ),
