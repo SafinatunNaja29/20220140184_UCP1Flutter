117929155552e293aa33ab3553841107940004a9
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-04-25 14:09:11 +0700

    membuat class RegisterPage

---
+++ lib/navigation/registerpage.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  const Registerpage({super.key});
+
+  @override
+  State<Registerpage> createState() => _RegisterpageState();
+}
