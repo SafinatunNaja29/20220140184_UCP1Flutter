785137b9124b4b6cd1792b22c3d6f2198ecb8f33
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-04-25 13:51:13 +0700

    membuat class LoginPage

---
+++ lib/navigation/loginpage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:ucp1/navigation/homepage.dart';
+import 'package:ucp1/navigation/registerpage.dart';
+
+class LoginPage extends StatefulWidget { 
+  const LoginPage({super.key}); 
+
+  @override
+  State<LoginPage> createState() => _LoginPageState(); 
+}
