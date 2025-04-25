57b35cda55fcf31f57e9787b7246901493090756
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-04-25 14:12:50 +0700

    membuat RegisterpageState untuk mengatur isi TextFormField

--- lib/navigation/registerpage.dart
+++ lib/navigation/registerpage.dart
@@ -6,3 +6,16 @@ class RegisterPage extends StatefulWidget {
   @override
   State<Registerpage> createState() => _RegisterpageState();
 }
+
+class _RegisterpageState extends State<Registerpage> {
+  final TextEditingController namaController = TextEditingController();
+  final TextEditingController emailController = TextEditingController(); 
+  final TextEditingController noHpController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController konfirmpasswordController = TextEditingController();
+
+
+  @override
+  void initState() {
+    super.initState();
+  }
