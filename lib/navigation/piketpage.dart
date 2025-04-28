187a50752692d5fa52dda9d331fd282790377bae
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-04-28 22:50:01 +0700

    Menambahkan fungsi tambah tugas, pilih tanggal, dan navigasi ke detail piket

--- lib/navigation/piketpage.dart
+++ lib/navigation/piketpage.dart
@@ -19,3 +19,35 @@ class _PiketPageState extends State<PiketPage> {
     super.dispose();
   }
 }
+
+void _addTugasPiket() {
+    if (_tugasController.text.isNotEmpty) {
+      setState(() {
+        _tugasPiketList.add(_tugasController.text);
+        _tugasController.clear();
+      });
+    }
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2100),
+    );
+    if (picked != null) {
+      setState(() {
+        _selectedDate = picked;
+      });
+    }
+  }
+
+  void _navigateToDetail(String tugas) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => DetailPiketPage(tugas: tugas),
+      ),
+    );
+  }
