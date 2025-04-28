05d0a97aba322e841da314303d8bcb823c6231e9
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-04-29 02:16:08 +0700

    membuat informasi transaksi dan tombol selesai

--- lib/navigation/detailbarang.dart
+++ lib/navigation/detailbarang.dart
@@ -15,3 +15,68 @@ class DetailBarangPage extends StatelessWidget {
     required this.jumlahBarang,
     required this.hargaSatuan,
   }) : super(key: key);
+
+ @override
+  Widget build(BuildContext context) {
+    int jumlah = int.tryParse(jumlahBarang) ?? 0;
+    int harga = int.tryParse(hargaSatuan) ?? 0;
+    int totalHarga = jumlah * harga;
+
+    return Scaffold(
+      backgroundColor: const Color(0xFFFEF7F5),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 24),
+              Container(
+                padding: const EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.green.withOpacity(0.2),
+                ),
+                child: const CircleAvatar(
+                  radius: 40,
+                  backgroundColor: Colors.green,
+                  child: Icon(Icons.check, color: Colors.white, size: 48),
+                ),
+              ),
+              const SizedBox(height: 24),
+              const Text(
+                "Data Berhasil Disimpan",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 32),
+              _buildDetailRow('Tanggal', tanggal),
+              _buildDetailRow('Jenis Transaksi', jenisTransaksi),
+              _buildDetailRow('Jenis Barang', jenisBarang),
+              _buildDetailRow('Jumlah Barang', jumlahBarang),
+              _buildDetailRow('Jenis Harga Satuan', 'Rp. $hargaSatuan'),
+              _buildDetailRow('Total Harga', 'Rp. $totalHarga'),
+              const Spacer(),
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.deepOrange,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).popUntil((route) => route.isFirst);
+                  },
+                  child: const Text(
+                    'Selesai',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
