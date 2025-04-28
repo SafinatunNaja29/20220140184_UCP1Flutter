7c65811910de25a1f5ec5c3806ccfa986374ef69
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-04-28 22:51:11 +0700

    Membuat struktur awal halaman Piket Gudang dengan AppBar dan body

--- lib/navigation/piketpage.dart
+++ lib/navigation/piketpage.dart
@@ -51,3 +51,15 @@ void _addTugasPiket() {
       ),
     );
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Piket Gudang'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
