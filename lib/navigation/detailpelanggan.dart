574fd7c3a9c839522eea84b6c0408b98067f38de
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-04-29 01:06:06 +0700

    Buat tampilan awal Scaffold dengan AppBar untuk halaman detail pelanggan

--- lib/navigation/detailpelanggan.dart
+++ lib/navigation/detailpelanggan.dart
@@ -7,6 +7,8 @@ class DetailPelangganPage extends StatelessWidget {
   final String alamatCust;
   final String provinsiCust;
   final String kodePosCust;
+  final Color primaryColor = const Color.fromARGB(255, 75, 139, 241);
+
 
   const DetailPelangganPage({
     Key? key,
@@ -17,3 +19,16 @@ class DetailPelangganPage extends StatelessWidget {
     required this.provinsiCust,
     required this.kodePosCust,
   }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFFEF7F5),
+      appBar: AppBar(
+        backgroundColor: primaryColor,
+        title: Text('Detail $namaCust'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
